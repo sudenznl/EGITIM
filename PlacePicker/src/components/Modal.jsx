e6c45a9cc6dc0a62b6990a6225d98ee6bce0a0fb
Sude ÜNLÜ <sudenznl@gmail.com>
2025-11-04 16:27:30 +0300

    new project - PlacePicker

---
+++ PlacePicker/src/components/Modal.jsx
@@ -0,0 +1,16 @@
+import { useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ open, children }) {
+  const dialog = useRef();
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} open={open}> 
+      {children}
+    </dialog>,
+    document.getElementById('modal')
+  );
+}
+
+export default Modal;
+ 
