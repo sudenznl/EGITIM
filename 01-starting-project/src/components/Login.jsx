4450c84e7c8b1b14a994d9ed3e8bda2463c80cf1
Sude ÜNLÜ <sudenznl@gmail.com>
2025-11-15 21:48:53 +0300

    fixed

--- 01-starting-project/src/components/Login.jsx
+++ 01-starting-project/src/components/Login.jsx
@@ -1,23 +1,50 @@
+import { useRef } from 'react';
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  //giriş düğmesi için oturum açma bileşenine fonksiyon ekliyoruz:
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    console.log(enteredEmail, enteredPassword);
+  }
+
   return (
-    <form>
-      <h2>Login</h2>
+    <form onSubmit={handleSubmit}>
+      <h2>Giriş Alanı</h2>
 
       <div className="control-row">
         <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          {/*htmlFor : html'deki for ve label-inpt ilişkilendirmek için kullanılır.*/}
+          <label htmlFor="email">E-mail</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            ref={email}
+          />
         </div>
 
         <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <label htmlFor="password">Şifre</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            ref={password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button button-flat">Sıfırla</button>
+        <button className="button">
+          Giriş
+        </button>
       </p>
     </form>
   );
