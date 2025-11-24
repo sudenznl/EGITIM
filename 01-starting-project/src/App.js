1a9aee3c22659e427163ee1043883ddcf4ba5aae
Sude ÜNLÜ <sudenznl@gmail.com>
2025-11-24 15:33:57 +0300

    new Project - Redux Counter

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react/jsx-runtime";
+
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      <Auth />
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
