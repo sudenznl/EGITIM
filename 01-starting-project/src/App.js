4cef81e62b2ae57765f49cc13fec85e144c1b7ef
Sude ÜNLÜ <sudenznl@gmail.com>
2025-11-27 11:01:24 +0300

    finishing

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,34 +1,36 @@
 import {
   createBrowserRouter,
-  //createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  //Route
-} from "react-router-dom";
+  // Route,
+} from 'react-router-dom';
 
-import HomePage from "./pages/Home";
-import ProductsPage from "./pages/Products";
-import RootLayout from "./pages/Root";
-import ErrorPage from "./pages/Error";
+import ErrorPage from './pages/Error';
+import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
-/*const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/"  element={<HomePage />} />
-    <Route path="/products"  element={<ProductsPage />} />
-  </Route>
-);*/
+     // const routeDefinitions = createRoutesFromElements(
+    //   <Route> 
+   //     <Route path="/" element={<HomePage />} /> 
+  //     <Route path="/products" element={<ProductsPage />} />
+ //   </Route>
+// );
 
 const router = createBrowserRouter([
   {
-    path    : '/',
-    element : <RootLayout />,
+    path: '/',
+    element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> },
+      { index: true, element: <HomePage /> },
+      { path: 'products', element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> }
     ],
-  },
+  }
 ]);
- 
+
 // const router = createBrowserRouter(routeDefinitions);
 
 function App() {
--- 01-starting-project/src/pages/Home.js
+++ 01-starting-project/src/pages/Home.js
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
-    return (
-        <>
-            <h1>Home Page</h1>
-            <p>
-                <Link to="/products">ürünler listesine</Link> git.
-            </p>
-        </>
-    );
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
+
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="products">the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
